a5b18049bb41d0c7c7dd162d6d6b68dcb949cf5d
Dezmone Musgrove <Dezmone.Musgrove@gmail.com>
2022-01-11 02:22:33 -0600

    views almost done

--- src/main/java/com/dezzycode/dateblog/controllers/HomeController.java
+++ src/main/java/com/dezzycode/dateblog/controllers/HomeController.java
@@ -1,8 +1,8 @@
 package com.dezzycode.dateblog.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomeController {
@@ -11,5 +11,22 @@ public class HomeController {
     @ResponseBody
     public String hello(){
         return "<h1>Welcome to Date Blog!</h1>" + "<br><h3>We remember that special moment!</h3>";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+
+    @GetMapping("/join")
+    public String showJoinForm() {
+        return "join";
+    }
+
+    @PostMapping("/join")
+    public String joinCohort(@RequestParam(name = "cohort") String cohort, Model model) {
+        model.addAttribute("cohort", "Welcome to " + cohort + "!");
+        return "join";
     }
 }
